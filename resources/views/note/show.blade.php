da1617f9381ce476a0581063918621faf7b8515b
ps_krutov_i <krutov.ivan@place-start.ru>
2025-06-08 10:23:58 +0300

    добавлена кнопка назад

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -1,13 +1,14 @@
 <x-lauout>
     <div class="note-container single-note">
         <div class="note-header">
-            <h1>Заметка: {{ $note->created_at }}</h1>
+            <h1>Заметка от: {{ (new DateTime(($note->created_at)))->format('d.m.Y') }}</h1>
             <div class="note-buttons">
+                <a href="{{ route('note.index') }}" class="note-cancel-button">Назад</a>
                 <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Редактировать</a>
                 <form action="{{ route('note.destroy', $note) }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <button class="note-delete-button">Delete</button>
+                    <button class="note-delete-button">Удалить</button>
                 </form>
             </div>
         </div>
