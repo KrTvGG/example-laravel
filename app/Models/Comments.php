a03b7f5751d576d9aee8b25020416fcb437fc964
ps_krutov_i <krutov.ivan@place-start.ru>
2025-06-13 21:32:45 +0300

    начало создания комментариев

---
+++ app/Models/Comments.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Comments extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'comment',
+        'note_id'
+    ];
+
+    protected $quarded = false;
+}
